5bd6415c2b2ba6504af375daf27191eca9de243f
Abhay Amin <pixoloproductions@gmail.com>
2025-11-25 21:54:33 +0530

    Development (#25)
    * Add HTML structure and mobile CSS for Story section
    
    * Revert "made the html structure for the testimonial-card"
    
    * Added container class
    
    * testimonial structure and section added
    
    * Removed font-sans from p tag
    
    * tailwind prettier
    
    * added  CSS in testimonial-card-component (#16)
    
    * added the required CSS in testimonial-card-component
    
    * fixed the changes
    
    * fixed the required changes
    
    * fixed the changes
    
    * testimonial section
    
    ---------
    
    Co-authored-by: Mohdhussainsunesara <m.sunesara@somaiya.edu>
    Co-authored-by: Abhay Amin <contactabhay2@gmail.com>
    
    * Testimonial Section added
    
    * Added the About Section in About Us Page (#20)
    
    Co-authored-by: Abhay Amin <contactabhay2@gmail.com>
    
    * Sheet Content Fixes (#21)
    
    * sheet fixes done
    
    * Testimonials new content
    
    ---------
    
    Co-authored-by: Abhay Amin <contactabhay2@gmail.com>
    
    * Fixes build issue
    
    * Fixes in Testimonials
    
    * Updated LinkedIn link in Contact section
    
    * Fixed Nav tabs for mobile and tablet (#24)
    
    * Fixed Nav tabs for mobile and tablet
    
    * Fixed comments on pr
    
    * Bugfix/headers cash display (#22)
    
    * Updated font of headers and sub-headers
    
    * Fixed requested change
    
    * Added Clash display font
    
    * Updated fonts
    
    * Changed file name to kebab casing and removed font-sans
    
    * Set global font-weight 600 for all heading tags
    
    * fonts fixes done
    
    ---------
    
    Co-authored-by: Mohdhussainsunesara <m.sunesara@somaiya.edu>
    
    ---------
    
    Co-authored-by: nagdevtaniya <nagdevtaniya@gmail.com>
    Co-authored-by: Mohd hussain <90891392+MohdhussainSunesara@users.noreply.github.com>
    Co-authored-by: Mohdhussainsunesara <m.sunesara@somaiya.edu>
    Co-authored-by: Abhay Amin <contactabhay2@gmail.com>
    Co-authored-by: PalakLund-06 <palaklund123@gmail.com>

---
+++ src/data/testimonials.ts
@@ -0,0 +1,53 @@
+export const testimonials = [
+  {
+    image_Url: "/images/testimonials/nishant.png",
+    name: "Vijay Sokhi",
+    job_Designation: "Director - Sharp Consulting & Implementing Company",
+    description:
+      "Rahul is a thoughtful person who has extreme clarity of what is expected out of him. He has elegantly made an ad film for a client of mine which was just a class apart. Rahul is a bankable person who can deliver more than what is expected.",
+  },
+  {
+    image_Url: "/images/testimonials/vijay.png",
+    name: "Nishant Jethva",
+    job_Designation: "Founder, Post Boy Studios",
+    description:
+      "Working with Rahul Yadav is always inspiring. As a director, he blends creativity, dedication, and versatility seamlessly. He doesn’t just execute a script — he elevates it with imagination and depth. With a keen visual sense and “jugaadu” problem-solving, Rahul brings every frame to life without compromising on quality. His commitment, eye for detail, and collaborative spirit make him a director you can truly trust.",
+  },
+
+  {
+    image_Url: "/images/testimonials/kaveri.png",
+    name: "Kaveri Mehrotra",
+    job_Designation:
+      "Creative Consultant, Director & Producer – Series Films and Documentary",
+    description:
+      "Rahul is a problem solver and has been a support in challenging situations in the project we collaborated on. Apart from directorial skills, he is able to take the load off the producer and handle logistics in the best way for the project. He is committed to be there till the bitter end and will make sure all aspects of the project are taken care of for a wholesome final delivery.",
+  },
+  {
+    image_Url: "/images/testimonials/mahesh.jpg",
+    name: "Mahesh Pednekar",
+    job_Designation: "Cinematographer",
+    description:
+      "Every project with Rahul turns into a creative adventure. His clarity of thought and deep respect for the crew make every frame a masterpiece in motion.",
+  },
+  {
+    image_Url: "/images/testimonials/abhijeet.jpg",
+    name: "Abhijeet Utekar",
+    job_Designation: "Producer",
+    description:
+      "Rahul’s vision and leadership on set are unmatched. His ability to bring out raw emotion while maintaining technical precision makes him an absolute joy to work with.",
+  },
+  {
+    image_Url: "/images/testimonials/abhay.jpg",
+    name: "Abhay Amin",
+    job_Designation: "Owner - Skorost United FC",
+    description:
+      "There are tasks you assign, and then there are moments that define your club. Rahul didn’t just organize a photo session, he captured the soul of Skorost. Every frame still speaks, every post still breathes the energy he helped create. A year later, we’re still using his work without a hint of repetition and that’s legacy, not just photography. Also the way he trained his matchday photographer, discipline, precision, passion... pure Skorost spirit. Rahul, your work wasn’t just done well, it was done beautifully.",
+  },
+  {
+    image_Url: "/images/testimonials/nishchay.jpg",
+    name: "Nishchay Ganotra",
+    job_Designation: "Business Director, Happenings Advertising",
+    description:
+      "I wholeheartedly recommend Rahul Yadav for any creative endeavor. As a Director and Creative Head, his ability to not only articulate a powerful vision but also inspire a team to execute it flawlessly is truly exceptional. His dedication to every project is unmatched; he approaches his work with a brilliant blend of creative innovation and strategic leadership. Rahul consistently delivers outstanding results",
+  },
+];
